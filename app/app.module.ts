a8cd4d2e94a57e270d8342d236949f962499c8b7
amalshehu <amalshehu@gmail.com>
2016-11-11 12:47:34 +0530

    Add declarations and provider

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { PostsComponent } from './posts.component';
+import { PostService } from './post.service';
+
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, PostsComponent ],
+  providers:[ PostService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
