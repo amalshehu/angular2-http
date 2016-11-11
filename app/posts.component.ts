bfaff53b3fcd205954b6762a26ca7bb55385e5ff
amalshehu <amalshehu@gmail.com>
2016-11-11 13:14:50 +0530

    Add html template

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -3,7 +3,14 @@ import { PostService } from './post.service';
 
 @Component({
     selector: 'posts',
-    template: `<h1>Posts</h1>`
+    template: `<h1>Posts</h1>
+              <ul>
+              <li *ngFor="let post of posts">
+              <h3>{{post.title}}</h3>
+              <p>{{post.body}}
+              </li>
+              </ul>
+    `
 })
 export class PostsComponent {
   private posts;
