ca6d123c6d066839966f8ca57a1d51c89d071552
amalshehu <amalshehu@gmail.com>
2016-11-11 12:46:54 +0530

    Add posts tag

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
+import { PostsComponent } from './posts.component';
+import { PostService } from './post.service';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    template: `<h1>Hello Angular</h1><posts></posts>`,
+
 })
-export class AppComponent { }
+
+export class AppComponent extends Type { }
