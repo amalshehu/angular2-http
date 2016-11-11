88ead652c371e775f5060ad3e38fc0d7718e4a77
amalshehu <amalshehu@gmail.com>
2016-11-11 15:47:53 +0530

    Add Forms module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { FormsModule }   from '@angular/forms';
 
 import { PostsComponent } from './posts.component';
 import { PostService } from './post.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, PostsComponent ],
   providers:[ PostService ],
   bootstrap:    [ AppComponent ]
