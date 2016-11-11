29801225283807bd65a264c23e5aa3fcc910fc01
amalshehu <amalshehu@gmail.com>
2016-11-11 15:44:59 +0530

    Change depricated code

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -4,6 +4,15 @@ import { PostService } from './post.service';
 @Component({
     selector: 'posts',
     template: `<h1>Posts</h1>
+              <form (submit) ="addPost()">
+              <label>Title</label>
+              <input [(ngModel)]="title" name="title">
+              <br>
+              <label>Body</label>
+              <input [(ngModel)]="body" name="body">
+              <br>
+              <input type = "submit" value = "Submit">
+              </form>
               <ul>
               <li *ngFor="let post of posts">
               <h3>{{post.title}}</h3>
@@ -14,7 +23,18 @@ import { PostService } from './post.service';
 })
 export class PostsComponent {
   private posts;
+  private title;
+  private body;
+  private newPost;
   constructor(private _postService:PostService){
-    this.posts = _postService.getPosts();
+    this.posts = this._postService.getPosts();
+  }
+  addPost(){
+
+  this.newPost = {
+    title:this.title,
+    body:this.body
+  }
+  this._postService.addPost(this.newPost);
   }
 }
